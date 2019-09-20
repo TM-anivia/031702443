1131ab3087e311a0bf285ddf6b123c7ad8c31542
TM_anivia <742961297@qq.com>
2019-09-21 02:06:36 +0800

    printt

--- 031702443.py
+++ 031702443.py
@@ -1,80 +1,86 @@
 import re
+import json
 
-cache = string = input('请输入地址：')
+cache = input('请输入地址：')
+
+dict = {}
+list = []
 
 print('{', end='')
 
+cache = re.sub('\d!','',cache)
+
 name = re.search('.+,', cache).group()  #提取名字
-name = re.search('[^,]+', name).group()
+dict["姓名"] = re.search('[^,]+', name).group()
 cache = re.sub('.+,', '', cache)
-print('"姓名":"%s"' % name, end=',')
 
-p_num = re.search('\d{11}', cache).group()  #提取号码
+dict["手机"]= re.search('\d{11}', cache).group()  #提取号码
 cache = re.sub('\d{11}', '', cache)
-print('"手机":"%s"' % p_num, end=',')
-
-print('"地址":[', end="")
 
 aaa = re.search('.{2}', cache).group()  #省
 flag = re.match('.+?省', cache)
 if aaa == '北京' or aaa == '天津' or aaa == '重庆' or aaa == '上海' :
-    print('"%s"' % aaa, end='')
+    list.append(aaa)
 elif flag != None :
     aaa = re.search('.+?省', cache).group()
     cache = cache.replace(aaa, '', 1)
-    print('"%s"' % aaa, end='')
+    list.append(aaa)
 else:
     aaa = re.search('..', cache).group()
     cache = cache.replace(aaa, '', 1)
-    print('"%s省"' % aaa, end='')
+    list.append(aaa+'省')
 
 flag = re.match('.+?市', cache)  #市
 if flag != None :
     aaa = re.search('.+?市', cache).group()
     cache = cache.replace(aaa, '', 1)
-    print(',"%s"' % aaa, end='')
+    list.append(aaa)
 else:
     aaa = re.search('..', cache).group()
     cache = cache.replace(aaa, '', 1)
-    print('"%s市"' % aaa, end='')
+    list.append(aaa+'市')
 
 flag = re.match('.+?(?:县|区)', cache)  #县/区
 if flag != None :
     aaa = re.search('.+?(?:县|区)', cache).group()
     cache = cache.replace(aaa, '', 1)
-    print(',"%s"' % aaa, end='')
+    list.append(aaa)
 else:
-    print(',""', end='')
+    list.append('')
 
-flag = re.match('..+?(?:镇|街道|乡)', cache)  #镇/街道/乡
+flag = re.match('..+?(?:镇|街道)', cache)  #镇/街道/乡
 if flag != None :
     aaa = re.search('..+?(?:镇|街道|乡)', cache).group()
     cache = cache.replace(aaa, '', 1)
-    print(',"%s"' % aaa, end='')
+    list.append(aaa)
 else:
-    print(',""', end='')
+    list.append('')
 
-flag = re.match('.+?(?:路|巷|街)', cache)  #路/巷/街
+flag = re.search('.+?(?:路|巷|街|道|乡)', cache)  #路/巷/街
 if flag != None :
-    aaa = re.search('.+?(?:路|巷|街)', cache).group()
+    aaa = re.search('.+?(?:路|巷|街|道|乡)', cache).group()
     cache = cache.replace(aaa, '', 1)
-    print(',"%s"' % aaa, end='')
+    list.append(aaa)
 else:
-    print(',""', end='')
+    list.append('')
 
-flag = re.match('.+?号', cache)  #号
-if flag != None :
-    aaa = re.search('.*?号', cache).group()
+flag = re.match('.+?(?:号|村)', cache).group()  #号
+flag1 = re.match('.+委会', cache).group()
+flag1 = flag1.replace(flag, '', 1)
+if (flag != None) and (flag1 != "委会") :
+    aaa = re.search('.+?(?:号|村)', cache).group()
     cache = cache.replace(aaa, '', 1)
-    print(',"%s"' % aaa, end='')
+    list.append(aaa)
 else:
-    print(',""', end='')
+    list.append('')
 
 flag = re.match('[^\.]+', cache)  #具体地址
 if flag != None :
     aaa = re.search('[^\.]+', cache).group()
-    print(',"%s"' % aaa, end='')
+    list.append(aaa)
 else:
-    print(',""', end='')
+    list.append('')
 
-print(']},')  #结束
+dict['地址'] = list
+j = json.dumps(dict,ensure_ascii=False)
+print(j)  #结束
